6049eb6d2a2749c4bc8c6711fa01180a7d7a5b3a
velugoti-tejaswi <94038821+velugoti-tejaswi@users.noreply.github.com>
2022-05-06 10:42:33 +0530

    added recipedetail serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -29,3 +29,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes', 
                   'price', 'link')
         read_only_fields = ('id',)
+        
+        
+class RecipeDetailSerializer(RecipeSerializer):
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
